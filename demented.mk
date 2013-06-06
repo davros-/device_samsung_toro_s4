ad52d9441839549897d1d0571b3903cb2b5cf20d
cphelps76 <cphelps76@gmail.com>
2013-06-05 23:32:03 -0500

    overlays and things

--- demented.mk
+++ demented.mk
@@ -25,10 +25,10 @@ OVERLAY_TARGET := pa_xhdpi
 $(call inherit-product-if-exists, device/samsung/toro/full_toro.mk)
 
 # cdma
-$(call inherit-product, vendor/carbon/config/common_cdma.mk)
+$(call inherit-product, vendor/demented/config/common_cdma.mk)
 
 # phone
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/demented/config/common_phone.mk)
 
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
@@ -36,10 +36,10 @@ PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA e
 # products
 PRODUCT_DEVICE := toro
 PRODUCT_BRAND := Google
-PRODUCT_NAME := carbon_toro
+PRODUCT_NAME := demented_toro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.toro.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=demented.toro.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
